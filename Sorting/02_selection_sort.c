56eb7963f18919e875c868e119e1a925b2824473
amritraj-dev <amritraj08oct@gmail.com>
2026-08-24 00:23:47 +0530

    Add selection sort

---
+++ Sorting/02_selection_sort.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+    int a[] = {64, 25, 12, 22, 11};
+    int n = 5;
+    for (int i = 0; i < n - 1; i++) {
+        int min = i;
+        for (int j = i + 1; j < n; j++)
+            if (a[j] < a[min]) min = j;
+        int t = a[i]; a[i] = a[min]; a[min] = t;
+    }
+    for (int i = 0; i < n; i++) printf("%d ", a[i]);
+    printf("\n");
+    return 0;
+}

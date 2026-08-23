70131c4a77626db6d6c15f2daed600e1f1c53c85
amritraj-dev <amritraj08oct@gmail.com>
2026-08-24 00:17:28 +0530

    Add array reversal implementation

---
+++ Arrays/05_reverse.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+    int arr[] = {10, 20, 30, 40, 50};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    for (int i = 0; i < n / 2; i++) {
+        int temp = arr[i];
+        arr[i] = arr[n - 1 - i];
+        arr[n - 1 - i] = temp;
+    }
+
+    printf("Reversed array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}

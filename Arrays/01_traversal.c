c9311d848e009351ef2bbddfbe13b8e12842835a
amritraj-dev <amritraj08oct@gmail.com>
2026-08-24 00:17:12 +0530

    Add array traversal implementation

---
+++ Arrays/01_traversal.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    int arr[] = {10, 20, 30, 40, 50};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Array elements: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    printf("\n");
+    return 0;
+}

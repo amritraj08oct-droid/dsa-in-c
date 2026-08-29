3c0a114150b1027c94f81d01cec3b7b4e3786c55
Amrit Raj <amritraj08oct@gmail.com>
2026-08-29 10:04:13 +0530

    Improve bubble sort with user input and early exit

--- Sorting/01_bubble_sort.c
+++ Sorting/01_bubble_sort.c
@@ -1,19 +1,38 @@
 #include <stdio.h>
 
-int main(void) {
-    int a[] = {5, 1, 4, 2, 8};
-    int n = 5;
+int main() {
+    int n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
     for (int i = 0; i < n - 1; i++) {
         int swapped = 0;
-        for (int j = 0; j < n - 1 - i; j++) {
-            if (a[j] > a[j + 1]) {
-                int t = a[j]; a[j] = a[j + 1]; a[j + 1] = t;
+
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
                 swapped = 1;
             }
         }
-        if (!swapped) break;
+
+        if (!swapped)
+            break;
     }
-    for (int i = 0; i < n; i++) printf("%d ", a[i]);
+
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
     printf("\n");
+
     return 0;
 }

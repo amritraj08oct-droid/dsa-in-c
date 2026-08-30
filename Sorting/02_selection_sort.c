309c20080a9fca7f91773e0fd7440c6fb4d1ef49
Amrit Raj <amritraj08oct@gmail.com>
2026-08-30 17:00:35 +0530

    Improve selection sort with user input and cleaner output

--- Sorting/02_selection_sort.c
+++ Sorting/02_selection_sort.c
@@ -1,15 +1,38 @@
 #include <stdio.h>
 
-int main(void) {
-    int a[] = {64, 25, 12, 22, 11};
-    int n = 5;
-    for (int i = 0; i < n - 1; i++) {
-        int min = i;
-        for (int j = i + 1; j < n; j++)
-            if (a[j] < a[min]) min = j;
-        int t = a[i]; a[i] = a[min]; a[min] = t;
+int main() {
+    int n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
-    for (int i = 0; i < n; i++) printf("%d ", a[i]);
+
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        if (minIndex != i) {
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
     printf("\n");
+
     return 0;
 }

10bf613d2a97e019c2e47f39fec505bc74159806
Amrit Raj <amritraj08oct@gmail.com>
2026-08-31 22:26:05 +0530

    Improve insertion sort with user input

--- Sorting/03_insertion_sort.c
+++ Sorting/03_insertion_sort.c
@@ -1,14 +1,33 @@
 #include <stdio.h>
 
-int main(void) {
-    int a[] = {12, 11, 13, 5, 6};
-    int n = 5;
-    for (int i = 1; i < n; i++) {
-        int key = a[i], j = i - 1;
-        while (j >= 0 && a[j] > key) { a[j + 1] = a[j]; j--; }
-        a[j + 1] = key;
+int main() {
+    int n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
-    for (int i = 0; i < n; i++) printf("%d ", a[i]);
+
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
     printf("\n");
+
     return 0;
 }

0135171dfdc4c5064b72b31b607268d551caac28
Amrit Raj <amritraj08oct@gmail.com>
2026-09-01 22:27:31 +0530

    Improve merge sort with user input

--- Sorting/04_merge_sort.c
+++ Sorting/04_merge_sort.c
@@ -1,26 +1,53 @@
 #include <stdio.h>
 
-void merge(int a[], int l, int m, int r) {
-    int temp[100], i = l, j = m + 1, k = 0;
-    while (i <= m && j <= r) temp[k++] = (a[i] < a[j]) ? a[i++] : a[j++];
-    while (i <= m) temp[k++] = a[i++];
-    while (j <= r) temp[k++] = a[j++];
-    for (i = l, k = 0; i <= r; i++, k++) a[i] = temp[k];
+void merge(int arr[], int left, int mid, int right) {
+    int temp[right - left + 1];
+    int i = left, j = mid + 1, k = 0;
+
+    while (i <= mid && j <= right) {
+        if (arr[i] <= arr[j])
+            temp[k++] = arr[i++];
+        else
+            temp[k++] = arr[j++];
+    }
+
+    while (i <= mid)
+        temp[k++] = arr[i++];
+
+    while (j <= right)
+        temp[k++] = arr[j++];
+
+    for (i = left, k = 0; i <= right; i++, k++)
+        arr[i] = temp[k];
 }
 
-void mergeSort(int a[], int l, int r) {
-    if (l >= r) return;
-    int m = l + (r - l) / 2;
-    mergeSort(a, l, m);
-    mergeSort(a, m + 1, r);
-    merge(a, l, m, r);
+void mergeSort(int arr[], int left, int right) {
+    if (left >= right)
+        return;
+
+    int mid = left + (right - left) / 2;
+    mergeSort(arr, left, mid);
+    mergeSort(arr, mid + 1, right);
+    merge(arr, left, mid, right);
 }
 
-int main(void) {
-    int a[] = {38, 27, 43, 3, 9, 82, 10};
-    int n = 7;
-    mergeSort(a, 0, n - 1);
-    for (int i = 0; i < n; i++) printf("%d ", a[i]);
+int main() {
+    int n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    mergeSort(arr, 0, n - 1);
+
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
     printf("\n");
+
     return 0;
 }
